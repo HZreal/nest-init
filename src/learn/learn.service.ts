c17e2096a4628aa4c6e81017a51a687512642415
HZreal <hzreal0823@gmail.com>
2023-09-14 16:01:44 +0800

    useTransaction

--- src/learn/learn.service.ts
+++ src/learn/learn.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { User } from './learn.entity';
 
 @Injectable()
@@ -8,7 +8,31 @@ export class LearnService {
     constructor(
         @InjectRepository(User)
         private usersRepository: Repository<User>,
+        private dataSource: DataSource,
     ) {}
+
+    /**
+     * 使用事务
+     * @param users
+     */
+    async useTransaction(users: any[]) {
+        const queryRunner = this.dataSource.createQueryRunner();
+
+        await queryRunner.connect();
+        await queryRunner.startTransaction();
+        try {
+            await queryRunner.manager.save(users[0]);
+            await queryRunner.manager.save(users[1]);
+
+            await queryRunner.commitTransaction();
+        } catch (err) {
+            // since we have errors lets rollback the changes we made
+            await queryRunner.rollbackTransaction();
+        } finally {
+            // you need to release a queryRunner which was manually instantiated
+            await queryRunner.release();
+        }
+    }
 
     async findAll(): Promise<User[]> {
         return this.usersRepository.find();
